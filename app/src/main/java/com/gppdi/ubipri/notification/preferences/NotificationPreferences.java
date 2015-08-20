1ab5f57f8549900fbaf55ee3dba9eedfeeebd54f
Wagner Kolberg <wkolberg@inf.ufrgs.br>
2015-08-20 10:40:28 -0300

    Initial GCM code to receive notifications

---
+++ app/src/main/java/com/gppdi/ubipri/notification/preferences/NotificationPreferences.java
@@ -0,0 +1,9 @@
+package com.gppdi.ubipri.notification.preferences;
+
+
+public class NotificationPreferences {
+
+    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+
+}
---
+++ app/src/main/java/com/gppdi/ubipri/notification/services/NotificationInstanceIDListenerService.java
@@ -0,0 +1,27 @@
+package com.gppdi.ubipri.notification.services;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.google.android.gms.iid.InstanceID;
+import com.google.android.gms.iid.InstanceIDListenerService;
+
+
+public class NotificationInstanceIDListenerService extends InstanceIDListenerService {
+
+    private static final String TAG = "NotificationInstanceIDLS";
+
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. This call is initiated by the
+     * InstanceID provider.
+     */
+    @Override
+    public void onTokenRefresh() {
+        // Fetch updated Instance ID token and notify our app's server of any changes (if applicable).
+        Intent intent = new Intent(this, RegistrationIntentService.class);
+        startService(intent);
+    }
+}
