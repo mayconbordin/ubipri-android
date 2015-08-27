c8b336a35ecf0d2ae69df1b998f94836b70de6e8
Wagner Kolberg <wkolberg@inf.ufrgs.br>
2015-08-27 12:21:32 -0300

    Add tests for the notification class using the gson builder

---
+++ app/src/test/java/com/gppdi/ubipri/notification/NotificationTest.java
@@ -0,0 +1,29 @@
+package com.gppdi.ubipri.notification;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.gppdi.ubipri.BuildConfig;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+public class NotificationTest {
+
+    @Test
+    public void testNotificationFromJSON() {
+        final String json = "{ \"id\": 456, \"timestamp_in\": 3000, \"send_format\": \"SMS\", \"send_message\": \"SMS TEST\" }";
+        Gson gson = new GsonBuilder().create();
+        Notification notification = gson.fromJson(json, Notification.class);
+        assertTrue(notification.getEventId() == 456);
+        assertTrue(notification.getTimestamp() == 3000);
+        assertTrue(notification.getType().equals("SMS"));
+        assertTrue(notification.getMessage().equals("SMS TEST"));
+    }
+
+}
