294dc8981cef7413f74b564e363e646f223ed4a4
Wagner Kolberg <wkolberg@inf.ufrgs.br>
2015-08-28 14:23:29 -0300

    Optimize imports

--- app/src/main/java/com/gppdi/ubipri/notification/services/NotificationInstanceIDListenerService.java
+++ app/src/main/java/com/gppdi/ubipri/notification/services/NotificationInstanceIDListenerService.java
@@ -1,11 +1,7 @@
 package com.gppdi.ubipri.notification.services;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
-import android.util.Log;
 
-import com.google.android.gms.iid.InstanceID;
 import com.google.android.gms.iid.InstanceIDListenerService;
 
 
