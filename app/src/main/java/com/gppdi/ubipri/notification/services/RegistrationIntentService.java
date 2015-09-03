14810e2163b21af6e89c09bc94a6e8547190ff3a
Wagner Kolberg <wkolberg@inf.ufrgs.br>
2015-09-03 15:08:46 -0300

    Register the GCM token in the notification server

--- app/src/main/java/com/gppdi/ubipri/notification/services/RegistrationIntentService.java
+++ app/src/main/java/com/gppdi/ubipri/notification/services/RegistrationIntentService.java
@@ -1,20 +1,26 @@
 package com.gppdi.ubipri.notification.services;
 
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 import com.google.android.gms.gcm.GcmPubSub;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 import com.google.android.gms.iid.InstanceID;
-
 import com.gppdi.ubipri.R;
+import com.gppdi.ubipri.notification.api.ApiNotificationService;
 import com.gppdi.ubipri.notification.preferences.NotificationPreferences;
 
 import java.io.IOException;
+
+import javax.inject.Inject;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 
 public class RegistrationIntentService extends IntentService {
@@ -22,42 +28,39 @@ public class RegistrationIntentService extends IntentService {
     private static final String TAG = "RegIntentService";
     private static final String[] TOPICS = {"global"};
 
+    @Inject
+    ApiNotificationService apiNotificationService;
+
+    private final Context context;
+    private final SharedPreferences sharedPreferences;
+
     public RegistrationIntentService() {
         super(TAG);
+        context = this;
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
     }
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-
         try {
             // In the (unlikely) event that multiple refresh operations occur simultaneously,
             // ensure that they are processed sequentially.
             synchronized (TAG) {
-                // [START register_for_gcm]
-                // Initially this call goes out to the network to retrieve the token, subsequent calls
-                // are local.
-                // [START get_token]
-                InstanceID instanceID = InstanceID.getInstance(this);
+                // Initially this call goes out to the network to retrieve the token,
+                // subsequent calls are local.
+                InstanceID instanceID = InstanceID.getInstance(context);
                 String token = instanceID.getToken(getString(R.string.gcm_defaultSenderId),
                         GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
-                // [END get_token]
                 Log.i(TAG, "GCM Registration Token: " + token);
 
-                // TODO: Implement this method to send any registration to your app's servers.
+                // Send token registration to the notification server
                 sendRegistrationToServer(token);
 
                 // Subscribe to topic channels
                 subscribeTopics(token);
-
-                // You should store a boolean that indicates whether the generated token has been
-                // sent to your server. If the boolean is false, send the token to your server,
-                // otherwise your server should have already received the token.
-                sharedPreferences.edit().putBoolean(NotificationPreferences.SENT_TOKEN_TO_SERVER, true).apply();
-                // [END register_for_gcm]
             }
         } catch (Exception e) {
-            Log.d(TAG, "Failed to complete token refresh", e);
+            Log.e(TAG, "Failed to complete token refresh", e);
             // If an exception happens while fetching the new token or updating our registration data
             // on a third-party server, this ensures that we'll attempt the update at a later time.
             sharedPreferences.edit().putBoolean(NotificationPreferences.SENT_TOKEN_TO_SERVER, false).apply();
@@ -76,7 +79,22 @@ public class RegistrationIntentService extends IntentService {
      * @param token The new token.
      */
     private void sendRegistrationToServer(String token) {
-        // Add custom implementation, as needed.
+        // You should store a boolean that indicates whether the generated token has been
+        // sent to your server. If the boolean is false, send the token to your server,
+        // otherwise your server should have already received the token.
+        apiNotificationService.registerGcmToken(token, new Callback() {
+            @Override
+            public void success(Object o, Response response) {
+                sharedPreferences.edit().putBoolean(NotificationPreferences.SENT_TOKEN_TO_SERVER, true).apply();
+                Log.i(TAG, "GCM token successfully registered in the notification server");
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                sharedPreferences.edit().putBoolean(NotificationPreferences.SENT_TOKEN_TO_SERVER, false).apply();
+                Log.e(TAG, "Failed to register the GCM token in the notification server");
+            }
+        });
     }
 
     /**
@@ -85,13 +103,10 @@ public class RegistrationIntentService extends IntentService {
      * @param token GCM token
      * @throws IOException if unable to reach the GCM PubSub service
      */
-    // [START subscribe_topics]
     private void subscribeTopics(String token) throws IOException {
         for (String topic : TOPICS) {
             GcmPubSub pubSub = GcmPubSub.getInstance(this);
             pubSub.subscribe(token, "/topics/" + topic, null);
         }
     }
-    // [END subscribe_topics]
-
 }
